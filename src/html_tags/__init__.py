860a92231b133b654f3b4fe9630be3f81ee0992a
Mike Deufel <MDeufel13@gmail.com>
2026-04-19 00:46:10 +0000

    update: extras reference

--- src/html_tags/__init__.py
+++ src/html_tags/__init__.py
@@ -1,10 +1,13 @@
 """HTML/SVG generation via python functions."""
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __author__ = 'Deufel'
-from .template import Safe, unpack, is_tag, tag, render_attrs, render, html_doc, mk_tag, html_to_tag, Datastar, MeCSS, Pointer, Favicon
+from .template import Safe, unpack, is_tag, tag, render_attrs, render, html_doc, mk_tag, html_to_tag, Datastar, MeCSS, Pointer, Highlight, Color_type_css, Favicon, Layout
 __all__ = [
+    "Color_type_css",
     "Datastar",
     "Favicon",
+    "Highlight",
+    "Layout",
     "MeCSS",
     "Pointer",
     "Safe",
